20f7978fa18fb8145b7550478367f6bbff3774e1
Aleksey Kiselyov <AlecseyKiselyov@yandex.ru>
2021-06-15 22:05:30 +0300

    Add Hibernate

---
+++ src/main/java/com/FirstCrud/demo/Client.java
@@ -0,0 +1,51 @@
+package com.FirstCrud.demo;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long clientID;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(Long clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
